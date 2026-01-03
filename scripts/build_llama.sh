44ba019b4c94499a56a5714793ca13342bbe8650
Boligorla vengababu <boligorlavengababu@gmail.com>
2026-01-03 12:01:54 +0530

    Update build_llama.sh

--- scripts/build_llama.sh
+++ scripts/build_llama.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# Update packages
+pkg update && pkg upgrade -y
+
+# Install required tools
+pkg install -y git cmake clang
+
+# Clone llama.cpp
+git clone https://github.com/ggerganov/llama.cpp.git
+
+# Build llama.cpp
+cd llama.cpp
+make
